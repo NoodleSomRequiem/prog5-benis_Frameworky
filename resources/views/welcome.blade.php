889260ba8123491afa640740d3466b2c27076af8
NoodleSomRequiem <semvanveen@outlook.com>
2023-01-05 15:50:30 +0100

    first commit voor de herkansing, dit is wat ik tot nu toe had + een aantal aanpassingen

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="UTF-8">
+<title>Aritec</title>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="https://media-exp1.licdn.com/dms/image/C4E0BAQHUo_h0JGtwYw/company-logo_200_200/0/1606490589727?e=2159024400&v=beta&t=MznxbjFunN-3xUqfv2aTCkKzTL8AGNJ4VwoYb3oc1Wk" />
+    <title>CMGT HTML CSS</title>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+
+<header>jojo</header>
+
+<main>
+    <section id="homepage">
+        <h2>
+            Welcome!
+        </h2>
+
+
+
+        </p>
+    </section>
+</main>
+<footer>
+    <p>
+        Thank you for visiting the jojo website!
+    </p>
+    <p>
+        2022/2023.
+
+    </p>
+</footer>
+
+</body>
+</html>
